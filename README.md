c106854f504d7144601b8438c00dc8d142122cac
febryandana <febryandana.fd@gmail.com>
2021-09-13 19:18:58 +0700

    update startpage

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Minimal Startpage
+
 Template for a minimalistic startpage with a search bar, clock, and space for a lot of links that can be categorized. Made with HTML, CSS, and Javascript (no plugins).
 
 <p align="center">
@@ -8,9 +9,10 @@ Template for a minimalistic startpage with a search bar, clock, and space for a 
 </p>
 
 ## Features
-* Responsive and mobile friendly
-* Easy to customize
-* Quick navigation
-* Fast to load
-* No visual clutter
-* Works without Javascript
+
+- Responsive and mobile friendly
+- Easy to customize
+- Quick navigation
+- Fast to load
+- No visual clutter
+- Works without Javascript
--- js/main.js
+++ js/main.js
@@ -6,73 +6,83 @@ const clockInterval = 100;
 /**
  * Search engine query url
  */
-const searchEngineUrl = 'https://duckduckgo.com/?q=';
+const searchEngineUrl = "https://google.com/search?q=";
 
 const tabKeyCode = 9;
 const enterKeyCode = 13;
 const escapeKeyCode = 27;
-const searchBarElement = document.getElementById('search-bar');
-const clockElement = document.getElementById('clock');
-const formElement = document.getElementById('search-form');
+const searchBarElement = document.getElementById("search-bar");
+const clockElement = document.getElementById("clock");
+const formElement = document.getElementById("search-form");
 
 /**
  * Return a string containing the formatted current date and time.
  */
 function getDateTime() {
-    const dateTime = new Date();
-    let day = dateTime.getDate();
-    let month = dateTime.getMonth() + 1;
-    let hour = dateTime.getHours();
-    let minutes = dateTime.getMinutes();
-    let seconds = dateTime.getSeconds();
+  const dateTime = new Date();
+  let day = dateTime.getDate();
+  let month = dateTime.getMonth() + 1;
+  let hour = dateTime.getHours();
+  let minutes = dateTime.getMinutes();
+  let seconds = dateTime.getSeconds();
 
-    if (hour < 0) {
-        hour = 24 + hour;
-    }
+  if (hour < 0) {
+    hour = 24 + hour;
+  }
 
-    let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
-    let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+  let date =
+    (day < 10 ? "0" + day : day) +
+    "/" +
+    (month < 10 ? "0" + month : month) +
+    "/" +
+    dateTime.getFullYear();
 
-    return date + '\n' + time;
+  let time =
+    (hour < 10 ? "0" + hour : hour) +
+    ":" +
+    (minutes < 10 ? "0" + minutes : minutes) +
+    ":" +
+    (seconds < 10 ? "0" + seconds : seconds);
+
+  return date + "\n" + time;
 }
 
 function setClock() {
-    clockElement.innerText = getDateTime();
+  clockElement.innerText = getDateTime();
 }
 
 function search() {
-    let value = searchBarElement.value;
-    if (!value) {
-        return;
-    }
+  let value = searchBarElement.value;
+  if (!value) {
+    return;
+  }
 
-    if (value.startsWith('https://') || value.startsWith('http://')) {
-        window.location = value;
-    } else {
-        window.location = searchEngineUrl + encodeURIComponent(value);
-    }
+  if (value.startsWith("https://") || value.startsWith("http://")) {
+    window.location = value;
+  } else {
+    window.location = searchEngineUrl + encodeURIComponent(value);
+  }
 }
-
 
 setClock();
 
 setInterval(() => {
-    setClock();
+  setClock();
 }, clockInterval);
 
 searchBarElement.focus();
-searchBarElement.value = '';
+searchBarElement.value = "";
 
-formElement.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    search();
+formElement.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  search();
 });
 
-document.addEventListener('keypress', (event) => {
-    if (event.keyCode == escapeKeyCode) {
-        searchBarElement.blur();
-        searchBarElement.value = '';
-    } else if (event.keyCode != tabKeyCode && event.keyCode != enterKeyCode) {
-        searchBarElement.focus();
-    }
+document.addEventListener("keypress", (event) => {
+  if (event.key == escapeKeyCode) {
+    searchBarElement.blur();
+    searchBarElement.value = "";
+  } else if (event.key != tabKeyCode && event.key != enterKeyCode) {
+    searchBarElement.focus();
+  }
 });
